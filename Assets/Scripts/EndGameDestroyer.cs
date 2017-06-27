64c7892080c7f659cba2b33444e1e0a088345b57
Alyona Markina <alyona.markina.fipi@gmail.com>
2017-06-27 10:14:14 +0300

    Release

---
+++ Assets/Scripts/EndGameDestroyer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameDestroyer : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+			Application.LoadLevel("Menu");	
+		}
+}
+
+		
+	
+
+
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// 
+
+/// Скрипт главного меню
+/// 
+
+public class MenuScript : MonoBehaviour
+{
+	void OnGUI()
+	{
+		const int buttonWidth = 108;
+		const int buttonHeight = 50;
+
+		// Определяем место кнопки на экране:
+		// по оси X - в центре, по оси Y - 2/3 от высоты
+		Rect buttonRect = new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(4 * Screen.height / 5 ) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+		);
+
+		// Нарисуйте кнопку, чтобы начать игру
+		if(GUI.Button(buttonRect,"Start!"))
+		{
+			// По щелчку по кнопке, загрузите первый уровень.
+			// "Stage1" - название первой сцены, которую мы создали.
+			// Ее то мы и загрузим.
+			Application.LoadLevel("Stage1");
+		}
+	}
+}
